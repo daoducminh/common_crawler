407e79a327264fd73100f04b250ee73d1abe4b47
Minh Dao <daoducminh1997@gmail.com>
2026-04-05 22:42:51 +0700

    Add gold and gasoline price trackers

--- common_crawler/settings.py
+++ common_crawler/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "common_crawler.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = "common_crawler (+http://www.yourdomain.com)"
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36 Edg/146.0.0.0"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
